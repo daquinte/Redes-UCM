2dd4fb65c5fa58b9794cf9a1f053206173616f86
Usuario VMs <usuario_vms@localhost.localdomain>
2018-05-16 18:02:52 +0200

    Creado el server para el ejercicio 4, y el cliente que de momento es igual

--- Practica22/Practica2-Ej4_server.cpp
+++ Practica22/Practica2-Ej4_server.cpp
@@ -18,9 +18,10 @@ int main (int argc, char **argv) {
 
 	memset((void*) &hints, '\0', sizeof(struct addrinfo));
 	hints.ai_family = AF_INET;      //Porque es IPv4
-	hints.ai_socktype = SOCK_STREAM; // STREAM siempre es TCP
+	hints.ai_socktype = SOCK_DGRAM; // DGRAM siempre es UDP
 	
 	//argv[1] = direccion , argv[2] = puerto(host)
+	//Res se rellena con los parametros de argv
 	int rc = getaddrinfo(argv[1], argv[2], &hints, &res);
 
 	if (rc != 0) {
@@ -31,11 +32,23 @@ int main (int argc, char **argv) {
 
 
 	int sd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-
+	//BIND: Define la dirección en la que se va a escuchar
 	bind (sd, res->ai_addr, res->ai_addrlen);
 
+	//LISTEN: Pone el server socket en modo pasivo, esperando que el cliente se conecte
+	//socket propio, y luego un int que son las conexiones pendientes en la cola del socket
+	//Si llega un request y la cola está llena, da error de tipo "ECONNREFUSED"
+	if(listen(sd, LISTEN_BACKLOG) == -1){
+	  std::cout << "error listen: " <<  perror("listen") << std::endl;
+	}
 
-	//freeaddrinfo(res);
+	//ACCEPT: devuelve un socket nuevo que hace referencia a la conexión establecida (Es decir, tienes los dos sockets aqui)
+	//Extrae la primera conexion de la cola de conexiones pendientes, y la convierte en socket ya conectado.
+	//Es bloqueante, pero puedes usar select()
+	sockaddr* addr;
+	socklen_t* addrlen;
+	int socketCliente = accept(sd,addr, addrlen);
+	
 	
 	//Mientras el booleano salir sea falso se siguen mandando y recibiendo mensajes. Este solo se volverá true al darle a la Q
 	while(!salir) {
@@ -60,44 +73,8 @@ int main (int argc, char **argv) {
                       getnameinfo(&src_addr, addrlen, host, NI_MAXHOST, serv, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
 		      std::cout << s << " bytes de " << host << ":" << serv << std::endl;
 		}
-	       
 
-		int aux;
-
-		struct tm* strTiempo;
-		time_t tiempo;
-		time (&tiempo);
-		strTiempo = localtime(&tiempo); //Igualamos el struct del tiempo con el tiempo local
-
-		//Revisa el buffer desde el inicio
-		switch(buf[0]){
-
-		//Si se pulsa t se guarda la hora y se le envía al cliente (src_addr)
-		case 't':
-		  aux = strftime(buf, 256 ,"Son las: %H:%M:%S", strTiempo);
-		  sendto(sd, buf, aux, 0, (struct sockaddr *) &src_addr, addrlen);		
-		  break;
-
-		  //Si se pulsa la d se guarda el día, mes y año y se envia al cliente (src_addr)
-		case 'd':			
-		  aux = strftime(buf, 256 ,"Hoy es: %Y-%m-%d", strTiempo);
-		  sendto(sd, buf, aux, 0, (struct sockaddr *) &src_addr, addrlen);
-		  break;
-
-		  //Si se pulsa q termina el proceso del servidor
-		case 'q':
-		 salir = true;
-		  std::cout << "Saliendo...\n";
-		  break;
-
-		  //Si se pulsa otra tecla muestra las instrucciones de lo que se debe pulsar
-		default:
-		  std::cout << "Pulsa T para mostrar la hora, D para mostrar el día o Q para salir " << buf[0]<< "\n";
-		  break;
-		 		
-		}
-
-		//std::cout << s << " bytes de " << host << ":" << serv;
+	       sendto(sd, buf, aux, 0, (struct sockaddr *) &src_addr, addrlen);
    
 	  
 	}
