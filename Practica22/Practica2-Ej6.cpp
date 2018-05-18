f477f38f3571033e609eeb3c4a62974da8a1f859
Usuario VMs <usuario_vms@localhost.localdomain>
2018-05-18 19:43:15 +0200

    Ejercicio 6: se manda continuamente como si estuviese pulsando una tecla que no fuese t, d o q. Y si se pulsa lo que sea se cierra

--- Practica22/Practica2-Ej6.cpp
+++ Practica22/Practica2-Ej6.cpp
@@ -5,15 +5,108 @@
 #include <string.h>
 #include <iostream>
 #include <time.h>
+#include <unistd.h>
 
 
 class Thread{
 
+public:
   Thread(int socket){ threadSocket = socket;};
+  ~Thread(){};
+
+
+  void mandaMensaje(){
+    
+
+    //Mientras el booleano salir sea falso se siguen mandando y recibiendo mensajes. Este solo se volverá true al darle a la Q
+	while(!salir) {
+
+		char buf[256];
+
+		struct sockaddr src_addr;
+		socklen_t addrlen = sizeof(src_addr); 
+
+		char host [NI_MAXHOST];
+		char serv [NI_MAXSERV];
+		
+		//Recibe el input(el mensaje) del cliente (nosotros)
+		//s = nº de bytes
+		ssize_t s = recvfrom(threadSocket, buf, 255, 0, /*IP y puerto del otro extremo (variable de salida)*/ &src_addr, /*argumento de entrada/salida*/&addrlen);
+		
+		//Inicializa en lo último que se ha recibido
+		buf[s]='\0';
+		
+		//Si queremos mostrar en número de bytes se deberá hacer dentro de este if escribiendo el valor de s
+		//Si has recibido bytes, s no es cero
+		if (s != 0){
+                      getnameinfo(&src_addr, addrlen, host, NI_MAXHOST, serv, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
+		      std::cout << s << " bytes de " << host << ":" << serv << std::endl;
+		}
+
+		std::cout << "Thread: " << pthread_self() << "\n";
+	       
+
+		int aux;
+
+		struct tm* strTiempo;
+		time_t tiempo;
+		time (&tiempo);
+		strTiempo = localtime(&tiempo); //Igualamos el struct del tiempo con el tiempo local
+
+		//Revisa el buffer desde el inicio
+		switch(buf[0]){
+
+		//Si se pulsa la d se guarda el día, mes y año y se envia al cliente (src_addr)
+		case 'd':			
+		  aux = strftime(buf, 256 ,"Hoy es: %Y-%m-%d", strTiempo);
+		  sendto(threadSocket, buf, aux, 0, (struct sockaddr *) &src_addr, addrlen);
+		  break;
+
+		//Si se pulsa t se guarda la hora y se le envía al cliente (src_addr)
+		case 't':
+		  aux = strftime(buf, 256 ,"Son las: %H:%M:%S", strTiempo);
+		  sendto(threadSocket, buf, aux, 0, (struct sockaddr *) &src_addr, addrlen);		
+		  break;
+
+		  //Si se pulsa q termina el proceso del servidor
+		case 'q':
+		 salir = true;
+		  std::cout << "Saliendo...\n";
+		  break;
+
+		  //Si se pulsa otra tecla muestra las instrucciones de lo que se debe pulsar
+		default:
+		  std::cout << "Pulsa T para mostrar la hora, D para mostrar el día o Q para salir " << buf[0]<< "\n";
+		  break;
+		 		
+		}
+
+		//std::cout << s << " bytes de " << host << ":" << serv;
+   
+		sleep(5);	  
+
+	}
+  }
 
 private:
   int threadSocket;
+  bool salir = false;
 
+};
+
+//Método llama al envio de mensajes de cada thread al inicio de la rutina
+extern "C"
+{
+  void* start_routine(void* _thread){
+
+    Thread* t = static_cast<Thread*>(_thread);
+    t-> mandaMensaje();
+
+    delete t;
+
+    return 0;
+
+  }
 }
 
 //El argv por si solo no hace nada. Luego cuando lo ejecutes tienes que pasarle un parámetro que es el localhost y el puerto
@@ -21,7 +114,6 @@ private:
 int main (int argc, char **argv) {
 	struct addrinfo hints;
 	struct addrinfo *res;
-	bool salir = false;
 
 	memset((void*) &hints, '\0', sizeof(struct addrinfo));
 	hints.ai_family = AF_INET;      //Porque es IPv4
@@ -41,74 +133,33 @@ int main (int argc, char **argv) {
 
 	bind (sd, res->ai_addr, res->ai_addrlen);
 
+	//listen
+	listen(sd, 5);
 
-	//freeaddrinfo(res);
-	
-	//Mientras el booleano salir sea falso se siguen mandando y recibiendo mensajes. Este solo se volverá true al darle a la Q
-	while(!salir) {
-
-		char buf[256];
-		struct sockaddr src_addr;
-		socklen_t addrlen = sizeof(src_addr); 
-
-		char host [NI_MAXHOST];
-		char serv [NI_MAXSERV];
-		
-		//Recibe el input(el mensaje) del cliente (nosotros)
-		//s = nº de bytes
-		ssize_t s = recvfrom(sd, buf, 255, 0, /*IP y puerto del otro extremo (variable de salida)*/ &src_addr, /*argumento de entrada/salida*/&addrlen);
-		
-		//Inicializa en lo último que se ha recibido
-		buf[s]='\0';
-		
-		//Si queremos mostrar en número de bytes se deberá hacer dentro de este if escribiendo el valor de s
-		//Si has recibido bytes, s no es cero
-		if (s != 0){
-                      getnameinfo(&src_addr, addrlen, host, NI_MAXHOST, serv, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
-		      std::cout << s << " bytes de " << host << ":" << serv << std::endl;
-		}
-	       
-
-		int aux;
-
-		struct tm* strTiempo;
-		time_t tiempo;
-		time (&tiempo);
-		strTiempo = localtime(&tiempo); //Igualamos el struct del tiempo con el tiempo local
-
-		//Revisa el buffer desde el inicio
-		switch(buf[0]){
-
-		//Si se pulsa t se guarda la hora y se le envía al cliente (src_addr)
-		case 't':
-		  aux = strftime(buf, 256 ,"Son las: %H:%M:%S", strTiempo);
-		  sendto(sd, buf, aux, 0, (struct sockaddr *) &src_addr, addrlen);		
-		  break;
-
-		  //Si se pulsa la d se guarda el día, mes y año y se envia al cliente (src_addr)
-		case 'd':			
-		  aux = strftime(buf, 256 ,"Hoy es: %Y-%m-%d", strTiempo);
-		  sendto(sd, buf, aux, 0, (struct sockaddr *) &src_addr, addrlen);
-		  break;
-
-		  //Si se pulsa q termina el proceso del servidor
-		case 'q':
-		 salir = true;
-		  std::cout << "Saliendo...\n";
-		  break;
-
-		  //Si se pulsa otra tecla muestra las instrucciones de lo que se debe pulsar
-		default:
-		  std::cout << "Pulsa T para mostrar la hora, D para mostrar el día o Q para salir " << buf[0]<< "\n";
-		  break;
-		 		
-		}
-
-		//std::cout << s << " bytes de " << host << ":" << serv;
-   
-	  
-	}
 	freeaddrinfo(res);
+
+	//Inicializamos el conjunto de threads (5 en principio por ejemplo)
+	int numThreads = 5;
+	pthread_t threadID[numThreads]; //Conjunto de ids de cada thread para llamar a cada uno en el for
+
+	for(int i = 0; i < numThreads; i++)
+	  {
+	    pthread_attr_t attr;
+
+	    Thread* thread = new Thread(sd);
+
+	    pthread_attr_init (&attr);
+	    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	    pthread_create(&threadID[i], &attr, start_routine, static_cast<void*>(thread));
+
+
+	  }
+
+	char c;
+	std::cin >> c;
+	
+	
+	//freeaddrinfo(res);
 
 
 	return 0;
