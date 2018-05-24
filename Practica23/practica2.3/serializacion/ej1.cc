57dee32d3cf27866529b902fcdc1b8a89e6f2e5b
Usuario VMs <usuario_vms@localhost.localdomain>
2018-05-24 10:42:40 +0200

    Empezando el ejercicio 2 de la práctica 3

--- Practica23/practica2.3/serializacion/ej1.cc
+++ Practica23/practica2.3/serializacion/ej1.cc
@@ -12,7 +12,7 @@
 class Jugador: public Serializable
 {
 public:
-    Jugador(const char * _n, int16_t _x, int16_t _y):x(_x),y(_y)
+    Jugador(const char * _n, int16_t _x, int16_t _y):pos_x(_x),pos_y(_y)
     {
         strncpy(name, _n, 80);
     };
@@ -27,13 +27,29 @@ public:
     {
     }
 
-public:
-    char name[80];
+  int16 getPosX () {return pos_x;}
+  int16 getPosY () {return pos_y;}
 
-    int16_t x;
-    int16_t y;
+  char* getName () {return name;}
+
+
+private:
+   
+  int16_t pos_x;
+  int16_t pos_y;
+
+  char name[MAX_NAME];
+  static const size_t MAX_NAME = 20;
+
 };
 
 int main(int argc, char **argv)
 {
+
+  Jugador* player = new Jugador("player one", 12, 345);
+
+  std::fstream fs;
+  fs.open ("Jugador1.txt", /*No sé qué poner aquí.com*/);
+
+
 }
